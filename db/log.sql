243918b4a6ae46f40b776d36a7fbd78469959628
Anup Khanal <anup.khanal@reisystems.com>
2017-11-16 15:46:34 -0500

    initial commit

---
+++ db/log.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS ci_logs;
+CREATE TABLE ci_logs (
+    ip                      INT NOT NULL,
+    page                    VARCHAR(255) NOT NULL,
+    user_agent              VARCHAR(255) NOT NULL,
+    referrer                VARCHAR(255) NOT NULL,
+    logged                  TIMESTAMP NOT NULL
+                            default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+    memory                  INT UNSIGNED NOT NULL,
+    render_elapsed          FLOAT NOT NULL,
+    ci_elapsed              FLOAT NOT NULL,
+    controller_elapsed      FLOAT NOT NULL,
+    mysql_elapsed           FLOAT NOT NULL,
+    mysql_count_queries     TINYINT UNSIGNED NOT NULL,
+    mysql_queries           TEXT NOT NULL
+) ENGINE=ARCHIVE;
